ed9e35d0ba953b0c9507b864002df5df7377a725
lewt <lewt@lew.com>
2018-07-23 12:06:26 -0400

    Initial commit

---
+++ src/main/java/com/thomas/practice_file_3/PracticeFile3Application.java
@@ -0,0 +1,12 @@
+package com.thomas.practice_file_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeFile3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeFile3Application.class, args);
+	}
+}
